aea81fe4620b0c6c28d9a5ef3c070a8bbeda25fd
Kamesh Sampath <kamesh.sampath@hotmail.com>
2020-01-17 14:48:14 +0530

    db app

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO fruit (id,name,season) VALUES (nextval(hibernate_sequence), "Mango", "Spring");
+INSERT INTO fruit (id,name,season) VALUES (nextval(hibernate_sequence), "Strawberry", "Spring");
+INSERT INTO fruit (id,name,season) VALUES (nextval(hibernate_sequence), "Orange", "Winter");
+INSERT INTO fruit (id,name,season) VALUES (nextval(hibernate_sequence), "GrapeFruit", "Winter");
+INSERT INTO fruit (id,name,season) VALUES (nextval(hibernate_sequence), "Blueberry", "Summer");
