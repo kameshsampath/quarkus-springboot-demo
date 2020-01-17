175df6ec50f27832f41cef546b0863a4dc15a2b6
Kamesh Sampath <kamesh.sampath@hotmail.com>
2020-01-17 18:38:10 +0530

    optimizations

--- src/main/java/com/example/hellospringboot/data/Fruit.java
+++ src/main/java/com/example/hellospringboot/data/Fruit.java
@@ -11,36 +11,38 @@ import javax.persistence.Id;
 @Entity
 public class Fruit {
 
-    private Long id;
-    public String name;
-    public String season;
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE)
+  private Long id;
+  public String name;
+  public String season;
 
-    public Long getId() {
-        return id;
-    }
+  public Long getId() {
+    return id;
+  }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+  public void setId(Long id) {
+    this.id = id;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  public void setName(String name) {
+    this.name = name;
+  }
 
-    public String getSeason() {
-        return season;
-    }
+  public String getSeason() {
+    return season;
+  }
 
-    public void setSeason(String season) {
-        this.season = season;
-    }
+  public void setSeason(String season) {
+    this.season = season;
+  }
 
-    public String toString(){
-        return String.format("Fruit[id=%d, name='%s', season='%s']", id, name, season);
-    }
-    
+  public String toString() {
+    return String.format("Fruit[id=%d, name='%s', season='%s']", id, name, season);
+  }
+
 }
