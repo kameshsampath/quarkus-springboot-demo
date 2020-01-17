ed87132a5d0adf6dc74a9bac4b598e7dac0673ba
Kamesh Sampath <kamesh.sampath@hotmail.com>
2020-01-17 10:32:50 +0530

    Adding Rest controller

---
+++ src/main/java/com/example/hellospringboot/GeeterController.java
@@ -0,0 +1,17 @@
+package com.example.hellospringboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * GeeterController
+ */
+@RestController
+public class GeeterController {
+    
+    @GetMapping
+    public String hello(){
+        return "Hello from SpringBoot";
+    }
+    
+}
