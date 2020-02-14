6f3f2b6740b08ca4e03ece5ece82cea1159fe2c7
Kamesh Sampath <kamesh.sampath@hotmail.com>
2020-02-14 21:39:39 +0530

    upgraded to Postgresdb

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO fruit (id,name,season) VALUES (nextval(hibernate_sequence), "Mango", "Spring");
-INSERT INTO fruit (id,name,season) VALUES (nextval(hibernate_sequence), "Strawberry", "Spring");
-INSERT INTO fruit (id,name,season) VALUES (nextval(hibernate_sequence), "Orange", "Winter");
-INSERT INTO fruit (id,name,season) VALUES (nextval(hibernate_sequence), "GrapeFruit", "Winter");
-INSERT INTO fruit (id,name,season) VALUES (nextval(hibernate_sequence), "Blueberry", "Summer");
+INSERT INTO Fruit (id,name,season) VALUES (nextval('hibernate_sequence'), 'Mango', 'Spring');
+INSERT INTO Fruit (id,name,season) VALUES (nextval('hibernate_sequence'), 'Strawberry', 'Spring');
+INSERT INTO Fruit (id,name,season) VALUES (nextval('hibernate_sequence'), 'Orange', 'Winter');
+INSERT INTO Fruit (id,name,season) VALUES (nextval('hibernate_sequence'), 'GrapeFruit', 'Winter');
+INSERT INTO Fruit (id,name,season) VALUES (nextval('hibernate_sequence'), 'Blueberry', 'Summer');
