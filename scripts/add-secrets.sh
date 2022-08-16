f7bec1702c52f2e80168947affdae806526ed13e
Kamesh <kamesh.sampath@hotmail.com>
2022-08-16 21:39:20 +0530

    remove registry port

--- scripts/add-secrets.sh
+++ scripts/add-secrets.sh
@@ -4,9 +4,9 @@ set -euxo pipefail
 
 drone secret add --name maven_mirror_url --data 'http://nexus.infra:8081/repository/maven-public/' "${GITEA_USERNAME}/quarkus-springboot-demo"
 
-drone secret add --name destination_image --data "${REGISTRY_NAME}:${REGISTRY_PORT}/example/quarkus-springboot-demo" "${GITEA_USERNAME}/quarkus-springboot-demo"
+drone secret add --name destination_image --data "${REGISTRY_NAME}/example/quarkus-springboot-demo" "${GITEA_USERNAME}/quarkus-springboot-demo"
 
-drone secret add --name image_registry --data "${REGISTRY_NAME}:${REGISTRY_PORT}" "${GITEA_USERNAME}/quarkus-springboot-demo"
+drone secret add --name image_registry --data "${REGISTRY_NAME}" "${GITEA_USERNAME}/quarkus-springboot-demo"
 
 drone secret add --name image_registry_user --data "${IMAGE_REGISTRY_USER}" "${GITEA_USERNAME}/quarkus-springboot-demo"
 
--- scripts/update-secrets.sh
+++ scripts/update-secrets.sh
@@ -4,9 +4,9 @@ set -euxo pipefail
 
 drone secret update --name maven_mirror_url --data 'http://nexus.infra:8081/repository/maven-public/' "${GITEA_USERNAME}/quarkus-springboot-demo"
 
-drone secret update --name destination_image --data "${REGISTRY_NAME}:${REGISTRY_PORT}/example/quarkus-springboot-demo" "${GITEA_USERNAME}/quarkus-springboot-demo"
+drone secret update --name destination_image --data "${REGISTRY_NAME}/example/quarkus-springboot-demo" "${GITEA_USERNAME}/quarkus-springboot-demo"
 
-drone secret update --name image_registry --data "${REGISTRY_NAME}:${REGISTRY_PORT}" "${GITEA_USERNAME}/quarkus-springboot-demo"
+drone secret update --name image_registry --data "${REGISTRY_NAME}" "${GITEA_USERNAME}/quarkus-springboot-demo"
 
 drone secret update --name image_registry_user --data "${IMAGE_REGISTRY_USER}" "${GITEA_USERNAME}/quarkus-springboot-demo"
 
