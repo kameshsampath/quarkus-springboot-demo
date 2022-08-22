fd5e150f7528835a692ac9b7308121d1bb295470
Kamesh <kamesh.sampath@hotmail.com>
2022-08-22 11:30:37 +0530

    update the drone repo variable

--- scripts/add-secrets.sh
+++ scripts/add-secrets.sh
@@ -2,12 +2,12 @@
 
 set -euxo pipefail
 
-drone secret add --name maven_mirror_url --data 'http://nexus.infra:8081/repository/maven-public/' "${DRONE_GIT_REPO}"
+drone secret add --name maven_mirror_url --data 'http://nexus.infra:8081/repository/maven-public/' "${QSBD_GIT_REPO}"
 
-drone secret add --name destination_image --data "${REGISTRY_NAME}/example/quarkus-springboot-demo" "${DRONE_GIT_REPO}"
+drone secret add --name destination_image --data "${REGISTRY_NAME}/example/quarkus-springboot-demo" "${QSBD_GIT_REPO}"
 
-drone secret add --name image_registry --data "${REGISTRY_NAME}" "${DRONE_GIT_REPO}"
+drone secret add --name image_registry --data "${REGISTRY_NAME}" "${QSBD_GIT_REPO}"
 
-drone secret add --name image_registry_user --data "${IMAGE_REGISTRY_USER}" "${DRONE_GIT_REPO}"
+drone secret add --name image_registry_user --data "${IMAGE_REGISTRY_USER}" "${QSBD_GIT_REPO}"
 
-drone secret add --name image_registry_password --data "${IMAGE_REGISTRY_PASSWORD}" "${DRONE_GIT_REPO}"
+drone secret add --name image_registry_password --data "${IMAGE_REGISTRY_PASSWORD}" "${QSBD_GIT_REPO}"
--- scripts/rm-secrets.sh
+++ scripts/rm-secrets.sh
@@ -2,12 +2,12 @@
 
 set -euxo pipefail
 
-drone secret rm --name maven_mirror_url "${DRONE_GIT_REPO}"
+drone secret rm --name maven_mirror_url "${QSBD_GIT_REPO}"
 
-drone secret rm --name destination_image "${DRONE_GIT_REPO}"
+drone secret rm --name destination_image "${QSBD_GIT_REPO}"
 
-drone secret rm --name image_registry "${DRONE_GIT_REPO}"
+drone secret rm --name image_registry "${QSBD_GIT_REPO}"
 
-drone secret rm --name image_registry_user "${DRONE_GIT_REPO}"
+drone secret rm --name image_registry_user "${QSBD_GIT_REPO}"
 
-drone secret update --name image_registry_password "${DRONE_GIT_REPO}"
+drone secret update --name image_registry_password "${QSBD_GIT_REPO}"
